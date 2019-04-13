46c76cd301949c69f6590f739df18a2b4670b99e
Charles Koutcheme <charles.koutcheme@gmail.com>
2019-04-13 17:41:55 +0200

    linkedin and smooth scrolling
    linkedin and smooth scrolling

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleSidebar(x) {
   document.getElementById("sidebar").classList.toggle('active');
   x.classList.toggle("change");
 }
+
+function scrollToWelcome(anchor) {
+  var element = document.getElementById(anchor);
+  element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+}
+
+
