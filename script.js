9bdca728ec688d27cb78c12c5f8253429a15041b
Charles Koutcheme <charles.koutcheme@gmail.com>
2019-04-13 21:42:28 +0200

    added header color modif
    added header color modification when white background

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ function scrollToWelcome(anchor) {
 }
 
 
+/* COde to change the header when reaching skills */
+
+function changeHeader() {
+  var skills = document.getElementById('skills');
+  // returns the position with respect to the window not the document
+  var skillsOffsetY = skills.getBoundingClientRect().top;
+  
+  //TODO: change that to improve performance
+  if (skillsOffsetY <= 0) {
+    var name = document.getElementById('name');
+    name.style.color = "#17252A";
+    var toggle = document.getElementById('toggle-btn');
+    var spans = toggle.getElementsByTagName('span');
+    var i = 0;
+    var l = spans.length;
+    for (i = 0; i < l; i++)
+      spans[i].style.backgroundColor = "#17252A";
+
+  } else {
+    var name = document.getElementById('name');
+    name.style.color = 'white';
+    var toggle = document.getElementById('toggle-btn');
+    var spans = toggle.getElementsByTagName('span');
+    var i = 0;
+    var l = spans.length;
+    for (i = 0; i < l; i++)
+      spans[i].style.backgroundColor = 'white';
+  }
+}
+
+window.addEventListener("scroll", changeHeader);
